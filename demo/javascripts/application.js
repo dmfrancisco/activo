a05706d5a4edd59434d29c2356fe8eaf4957d53b
dfrancisco <dfrancisco@ipn.pt>
2010-09-17 11:07:02 +0100

    New branch for the webpage of the project

---
+++ demo/javascripts/application.js
@@ -0,0 +1,27 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function()
+{
+  // jQuery datepicker for formtastic (see http://gist.github.com/271377)
+  $('input.ui-datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
+
+  // Tooltips (see http://onehackoranother.com/projects/jquery/tipsy)
+  $('img').each( function() {
+    if ($(this).get(0).title != '') {
+      $(this).tipsy();
+    }
+  });
+});
+
+// Scroll effect for anchors
+jQuery(function( $ )
+{
+	$('a').click(function() {
+	   if ($(this).get(0).id == 'anchor') {
+		 $.scrollTo(this.hash, 500);
+		 $(this.hash).find('span.message').text(this.href);
+		 return false;
+	   }
+	});
+});
