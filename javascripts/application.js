2976e5906c3dd0e507961ccdec8fbabf614fc01d
Mirek Rusin <mirek@me.com>
2010-11-10 14:43:10 +0100

    Play nice with prototypejs

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,27 +1,31 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(document).ready(function()
-{
-  // jQuery datepicker for formtastic (see http://gist.github.com/271377)
-  $('input.ui-datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
+(function($) {
+  
+  $(document).ready(function()
+  {
+    // jQuery datepicker for formtastic (see http://gist.github.com/271377)
+    $('input.ui-datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
 
-  // Tooltips (see http://onehackoranother.com/projects/jquery/tipsy)
-  $('img').each( function() {
-    if ($(this).get(0).title != '') {
-      $(this).tipsy();
-    }
+    // Tooltips (see http://onehackoranother.com/projects/jquery/tipsy)
+    $('img').each( function() {
+      if ($(this).get(0).title != '') {
+        $(this).tipsy();
+      }
+    });
   });
-});
 
-// Scroll effect for anchors
-jQuery(function( $ )
-{
-	$('a').click(function() {
-	   if ($(this).attr('class') == 'anchor') {
-		   $.scrollTo(this.hash, 500);
-		   $(this.hash).find('span.message').text(this.href);
-		   return false;
-	   }
-	});
-});
+  // Scroll effect for anchors
+  jQuery(function( $ )
+  {
+  	$('a').click(function() {
+  	   if ($(this).attr('class') == 'anchor') {
+  		   $.scrollTo(this.hash, 500);
+  		   $(this.hash).find('span.message').text(this.href);
+  		   return false;
+  	   }
+  	});
+  });
+
+})(jQuery)
