63f1581175ff3056568d38cfd03fe171cd60a542
David Francisco <dfrancisco@ipn.pt>
2011-05-09 16:46:52 +0100

    Added second version of activo in a new branch

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,31 +1,26 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 (function($) {
-  
   $(document).ready(function()
   {
-    // jQuery datepicker for formtastic (see http://gist.github.com/271377)
+    // jQuery uniform controls (http://pixelmatrixdesign.com/uniform)
+    $("select, input:checkbox, input:radio, input:file").uniform();
+
+    // jQuery datepicker for formtastic (http://gist.github.com/271377)
     $('input.ui-datepicker').datepicker({ dateFormat: 'dd-mm-yy' });
 
-    // Tooltips (see http://onehackoranother.com/projects/jquery/tipsy)
+    // Tooltips (http://onehackoranother.com/projects/jquery/tipsy)
     $('img').each( function() {
       if ($(this).get(0).title != '') {
         $(this).tipsy();
       }
     });
-  });
 
-  // Scroll effect for anchors
-  jQuery(function( $ )
-  {
-  	$('a').click(function() {
-  	   if ($(this).attr('class') == 'anchor') {
-  		   $.scrollTo(this.hash, 500);
-  		   $(this.hash).find('span.message').text(this.href);
-  		   return false;
-  	   }
-  	});
+    // Scroll effect for anchors (http://flesler.blogspot.com/2007/10/jqueryscrollto.html)
+    $('a').click(function() {
+       if ($(this).attr('class') == 'anchor') {
+         $.scrollTo(this.hash, 500);
+         $(this.hash).find('span.message').text(this.href);
+         return false;
+       }
+    });
   });
-
 })(jQuery)
