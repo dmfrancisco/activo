fc3d662a03bb9d62be18f55446d09fd706add8f4
Tobias Schlottke <tobias.schlottke@gmail.com>
2012-01-31 11:03:54 +0100

    re-added selects to the uniform styling list

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,7 @@
   $(document).ready(function()
   {
     // jQuery uniform controls (http://pixelmatrixdesign.com/uniform)
-    $(".uniform input:checkbox, .uniform input:radio, .uniform input:file").uniform();
+    $(".uniform select, .uniform input:checkbox, .uniform input:radio, .uniform input:file").uniform();
 
     // jQuery datepicker for formtastic (http://gist.github.com/271377)
     $('input.ui-datepicker-input').datepicker({ dateFormat: 'dd-mm-yy' });
