a98c74a3f9185b0d09b8daacb1d697cdfc229517
Tobias Schlottke <tobias.schlottke@gmail.com>
2012-01-31 10:03:45 +0100

    revert changes. better updating to new uniform version

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,13 +3,6 @@
   {
     // jQuery uniform controls (http://pixelmatrixdesign.com/uniform)
     $(".uniform input:checkbox, .uniform input:radio, .uniform input:file").uniform();
-    
-    // uniform for select elements (do not match multi-selects)
-    $('.uniform select').each(function(){
-      if ($(this).attr('multiple') === undefined) {
-        $(this).uniform();
-      }
-    })
 
     // jQuery datepicker for formtastic (http://gist.github.com/271377)
     $('input.ui-datepicker-input').datepicker({ dateFormat: 'dd-mm-yy' });
